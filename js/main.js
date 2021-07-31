1265c4c96b6cc019e9f138e35b635b57d2906e29
Asliddin <asliddintukhtasinov5@gmail.com>
2021-07-31 18:54:30 +0500

    js scripte is remove in html file

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+const navMenu = document.querySelector(".nav-menu");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = ()=>{
+    menuBtn.classList.add("hide")
+    navMenu.classList.remove("hide");
+    navMenu.classList.add("nav-menu-show");
+};
+cancelBtn.onclick=()=>{
+    menuBtn.classList.remove("hide");
+    navMenu.classList.remove("nav-menu-show");
+    navMenu.classList.add("hide");
+}
+
 alert("Bu oddiy resposiv landing page ga qolib, Backend dasturchi(Asliddin) tomonidan o'rganish va mustahkamlash uchun bajarilgan");
 console.log("Asliddin Tuxtasinov");
